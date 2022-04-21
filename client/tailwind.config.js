7ec534376ef4a4e6d1e257aaeac9e6b6ff6f6716
Utpal Gupta <44098979+utpal-gupta58@users.noreply.github.com>
2022-04-21 22:45:27 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: ["./src/**/*.js"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: ["'Poppins'"],
+			mono: ["'Libre Baskerville'"],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
